13e7f28c161df6fd0c8fc10cd5864b3072d0b014
gpollep <gpollep@ncsu.edu>
2017-03-25 01:11:20 -0400

    Added request's protocol buffer template

--- Client/db_utils.py
+++ Client/db_utils.py
@@ -1,8 +1,9 @@
 import sqlite3
 
 con = None
+cur = None
 
-def create_table(cur):
+def create_table():
     sql_query = "CREATE TABLE CHUNKS (" \
         "ID INTEGER PRIMARY KEY AUTOINCREMENT,"\
         "HASH VARCHAR(40) NOT NULL,"\
@@ -14,21 +15,24 @@ def create_table(cur):
     except sqlite3.Error, e:
         print "Error : %s"% e.args[0]
 
-def add_chunk( cur, hash, filename, offset, length):
+def add_chunk(  hash, filename, offset, length):
     sql_query = "INSERT INTO CHUNKS (HASH,FILENAME,OFFSET,LEN) VALUES ('%s','%s',%d,%d);"%(hash,filename,offset,length)
     cur.execute(sql_query)
 
-def is_chunk_present(cur, hash):
+def is_chunk_present( hash):
     cur = con.cursor()
     sql_query = "SELECT * FROM CHUNKS WHERE HASH='%s';" % hash
     cur.execute(sql_query)
     return len(cur.fetchall())>0
 
+def init_db():
+    con = sqlite3.connect("files.db")
 
-con = sqlite3.connect("files.db")
+    with con:
+        cur = con.cursor()
+        return cur
 
-with con:
-    cur = con.cursor()
-    create_table(cur)
-    add_chunk(cur,"adas","a.txt",0,10)
-    print(is_chunk_present(cur,"adas"))
+init_db()
+create_table()
+add_chunk("adas","a.txt",0,10)
+print(is_chunk_present("adas"))
--- Client/hash_utils.py
+++ Client/hash_utils.py
@@ -1,8 +1,10 @@
 import hashlib
 
+
 def compute_hash(s):
     o = hashlib.sha1(s)
     return o.hexdigest()
+
 
 if __name__ == '__main__':
     s = "GuruDarshan"
