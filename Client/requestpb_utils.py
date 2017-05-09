974ea50872751994544f9ff16d1a4e08a605c8a8
gpollep <gpollep@ncsu.edu>
2017-05-09 06:47:20 -0400

    Added client IP into the requests

--- Client/requestpb_utils.py
+++ Client/requestpb_utils.py
@@ -3,11 +3,23 @@ from defines_pb2 import *
 import os.path
 from db_utils import *
 from file_splicer_utils import *
+import socket
 
 def get_parent_dir(filenm):
     if filenm=='/':
         return 'NO_ROOT'
     return os.path.abspath(os.path.join(filenm, os.pardir))
+
+def get_my_ip(nat=False):
+    if (nat):
+        return "152.7.99.61"
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    val = s.getsockname()[0]
+    s.close()
+    return val
+
 
 def request_file_hashes(reqid, filenm):
     r = Request()
@@ -15,6 +27,7 @@ def request_file_hashes(reqid, filenm):
     r.method = GETHASHES
     r.filename = filenm
     r.parent = get_parent_dir(filenm)
+    r.client_ip = get_my_ip()
     return r
 
 
@@ -24,6 +37,7 @@ def request_dir_info(reqid,dirnm):
     r.method = GETDIRLIST
     r.filename = dirnm
     r.parent = get_parent_dir(dirnm)
+    r.client_ip = get_my_ip()
     return r
 
 
@@ -33,6 +47,7 @@ def request_file_info(reqid,filenm):
     r.method = GETFILEINFO
     r.filename = filenm
     r.parent = get_parent_dir(filenm)
+    r.client_ip = get_my_ip()
     return r
 
 
@@ -42,6 +57,7 @@ def request_write_chunk(reqid,filenm):
     r.method = WRITECHUNK
     r.filename = filenm
     r.parent = get_parent_dir(filenm)
+    r.client_ip = get_my_ip()
 
     return r
 
@@ -51,6 +67,7 @@ def request_update_fileinfo(reqid,filenm,sz,m_time,is_dir):
     r.method = UPDATEFILEINFO
     r.filename = filenm
     r.parent = get_parent_dir(filenm)
+    r.client_ip = get_my_ip()
 
     finfo = r.fileinfo
     finfo.filename = filenm
@@ -66,6 +83,7 @@ def request_remove_file(reqid,filenm,is_dir):
     r.method = REMOVEFILE
     r.filename = filenm
     r.parent = get_parent_dir(filenm)
+    r.client_ip = get_my_ip()
 
     finfo = r.fileinfo
     finfo.filename = filenm
@@ -82,6 +100,7 @@ def request_add_dir(reqid,filenm):
     r.method = ADDDIR
     r.filename = filenm
     r.parent = get_parent_dir(filenm)
+    r.client_ip = get_my_ip()
 
     finfo = r.fileinfo
     finfo.filename = filenm
