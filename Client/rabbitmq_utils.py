4622271ca7f3a1577709cec8b3c68d33f525c0d1
gpollep <gpollep@ncsu.edu>
2017-05-09 08:03:37 -0400

    Updated IP address method

--- Client/rabbitmq_utils.py
+++ Client/rabbitmq_utils.py
@@ -2,15 +2,24 @@ import pika
 import json
 from geoip import geolite2
 from time import sleep
+import requests
 
 SCALEDOWN_FACTOR = 1000
+FREEGEOPIP_URL = 'http://freegeoip.net/json'
 
-def get_location(ip_addr):
+def get_location2(ip_addr):
     match = geolite2.lookup(ip_addr)
     if match is not None:
         return list(match.location)
     else:
         return [0.0,0.0]
+
+def get_location(ip_addr):
+    url = '{}/{}'.format(FREEGEOPIP_URL, ip_addr)
+    response = requests.get(url)
+    response.raise_for_status()
+    rj = response.json()
+    return [ float(rj["latitude"]) , float(rj["longitude"]) ]
 
 
 def add_to_transfer_queue(sender_ip,receivers_ip,n):
@@ -43,4 +52,4 @@ def add_to_transfer_queue(sender_ip,receivers_ip,n):
 
     connection.close()
 
-add_to_transfer_queue("152.7.99.61",["24.163.44.95","52.56.177.147"],10000)
+add_to_transfer_queue("152.7.99.61",["24.163.44.95","52.56.177.147","52.76.232.13"],20000)
